7ee3a06f8470eaa472353a324a47987211f19572
Kate <e.khizova@gmail.com>
2023-12-23 19:37:13 +0300

    feat: add vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -26,12 +26,13 @@ class VigenereCipheringMachine {
     this.type = type; 
   }
   encrypt(str, key) {
+    if (arguments.length <= 1 || typeof str !== 'string' || typeof key !== 'string')  {
+      throw new Error('Incorrect arguments!');
+    }
+
     let strOfLetters = str.replaceAll(' ', '');
     let newKey = key.padEnd(strOfLetters.length, key);
     
-    if (arguments.length === 0) {
-      throw new Error('Incorrect arguments!');
-    }
     let getCharechterNumbers = function(str) {
       let messageNums = [];
       let message = str.toUpperCase();
@@ -74,16 +75,65 @@ class VigenereCipheringMachine {
     }
     
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+    if (arguments.length <= 1 || typeof str !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let strOfLetters = str.replaceAll(' ', '');
+    let newKey = key.padEnd(strOfLetters.length, key);
+    
+    let getCharechterNumbers = function(str) {
+      let messageNums = [];
+      let message = str.toUpperCase();
+      for (let i = 0; i < message.length; i += 1) {
+        for (let j = 0; j < alphabet.length; j += 1) {
+          if (message[i] === alphabet[j]) {
+            messageNums.push(j);
+          } 
+        }
+      }
+      return messageNums;
+    }
+    let charachterNumbers = getCharechterNumbers(str);
+    let keyCharachterNumbers = getCharechterNumbers(newKey);
+
+    let encryptedNumbers = [];
+    for (let i = 0; i < charachterNumbers.length; i += 1) {
+      if ((charachterNumbers[i] - keyCharachterNumbers[i]) < 0) {
+        encryptedNumbers.push(((charachterNumbers[i] - keyCharachterNumbers[i]) + 26)%26);
+      }
+      encryptedNumbers.push(((charachterNumbers[i] - keyCharachterNumbers[i]))%26);
+    }
+
+    let encryptedStr = '';
+    for (let i = 0; i < encryptedNumbers.length; i += 1) {
+      for (let j = 0; j < alphabet.length; j += 1) {
+        if (j === encryptedNumbers[i]) {
+          encryptedStr += alphabet[j];
+        }
+      }
+    }
+
+    let res = encryptedStr.split('');
+    for (let i = 0; i < str.length; i += 1) {
+      if (!str[i].match(/[a-z]/i)) {
+        res.splice(i, 0, str[i]);
+      }
+    }
+    if (this.type === false) {
+      return res.reverse().join('');
+    } else {
+      return res.join('');
+    }
   }
 }
-const directMachine = new VigenereCipheringMachine();
+const directMachine = new VigenereCipheringMachine(false);
 
-console.log(directMachine.encrypt('attack at dawn!', 'alphonse'));
+console.log(directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse'));
 
 
 module.exports = {
   VigenereCipheringMachine
 };
+
